04bbe86889a5d896c04b1a6bc9fb40b28e18a2f5
knppkp <khawpun2547@gmail.com>
2025-01-02 04:18:37 +0700

    initial

--- server/database.py
+++ server/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# PostgreSQL connection URL
-DATABASE_URL = "postgresql://postgres:226232547@localhost/coboard"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
