ab8b6cc76985b9c319479c91a80c469b784b67a8
knppkp <khawpun2547@gmail.com>
2025-01-04 00:23:15 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import Main from './Main';
 import Board from './Board';
 import Forum from './Forum';
