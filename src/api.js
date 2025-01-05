fcaed0059a3d6e5b9f590b8e202d3dedcdc9222c
knppkp <khawpun2547@gmail.com>
2025-01-05 19:39:28 +0700

    initial

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://coboard-uwrz.onrender.com";
+const API_BASE_URL = "https://api.knppkp.me";
 
 export const fetchForums = async (board) => {
   try {
